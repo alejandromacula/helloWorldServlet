d7e0d2a6fe82779813dd92edb9809ac118c4f4d1
alejandromacula <alejandro_93_7@hotmail.com>
2015-05-13 21:12:22 -0300

    hello world servlet

---
+++ holaMundo/src/com/javatechig/HelloServlet.java
@@ -0,0 +1,45 @@
+package com.javatechig;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public void init() throws ServletException {
+		// Servlet initialization code here
+		super.init();
+	}
+
+	@Override
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+		// Set response content type
+		response.setContentType("text/html");
+
+		// Actual logic goes here.
+		PrintWriter out = response.getWriter();
+		out.println("<h1>HOTEL SOLUTIONS v.1 </h1>");
+		out.println("<h3>primeras pruebas con servlets </h3>");
+		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+
+	}
+
+	@Override
+	public void destroy() {
+		// resource release
+		super.destroy();
+	}
+}
